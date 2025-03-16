3e065a9e2fd3c155a9fa9df1b7577b34d99f6600
friskaaaaa <0110124022@student.nurulfikri.ac.id>
2025-03-16 19:27:34 +0700

    Praktikum 4

---
+++ lingkaran.php
@@ -0,0 +1,26 @@
+<?php
+class Lingkaran {
+    public $jari;
+    public const PHI = 3.14;
+
+    public function __construct($r) {
+        $this->jari = $r;
+    }
+    public function getluas() {
+        $luas = self::PHI * $this->jari * $this->jari;
+        return $luas;
+    }
+
+    public function getkeliling() {
+        $keliling = 2.0 * self::PHI * $this->jari;
+        return $keliling;
+    }
+
+    public function cetak(){
+        echo "Lingkaran dengan jari2 ".$this->jari;
+        echo "<br>Luas = ".$this->getLuas();
+        echo "<br>Keliling = ".$this->getkeliling();
+    }
+}
+
+?>
